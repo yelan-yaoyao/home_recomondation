d53e7de98dc93b2b1d2c339db75f112e7b895e68
yelan-yaoyao <jayehkumar322@gmail.com>
2025-01-13 11:32:46 +0530

    updated js file
    added pages of faq and explore

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log("hello this is test to check the link");
+// console.log("hello this is test to check the link");
+
+
+document.getElementById('faqbtn').addEventListener('click', function (){
+    window.location.href = '/pages/faq.html';
+});
+
+document.getElementById('expbtn').addEventListener('click', function (){
+    window.location.href = '/pages/houses.html';
+});
+
+
+
+
+
